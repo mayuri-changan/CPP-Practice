74d1bd4c1539b98b907fe8e9f7d59a9a29c8c74a
mayuri-changan <mayuri2001changan@gmail.com>
2026-06-06 22:55:23 +0530

    practice

---
+++ q2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i,j,n=5;
+    for(i=1;i<=n;i++)
+    {
+for(j=1;j<=n-i;j++)
+cout<<" ";
+for(j=1;j<=(2*i-1);j++)
+{
+    if(i==1||i==n||j==1||j==(2*i-1))
+    cout<<"*";
+    else
+    cout<<" ";
+}
+cout<<endl;
+    }
+    return 0;
+}
