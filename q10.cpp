75d15f9ba8f4b09589e7fb190ac02e042bc87ebe
mayuri-changan <mayuri2001changan@gmail.com>
2026-06-08 22:23:56 +0530

    q10

---
+++ q10.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int i,j,s,n=5;
+    for(i=1;i<=n;i++)
+    {
+        for(s=1;s<=n-i;s++)
+        cout<<" ";
+        for(j=1;j<=i;j++)
+        cout<<i<<" ";
+         cout<<endl;
+    }
+   
+}
+   
