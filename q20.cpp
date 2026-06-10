e1c69aa84893d11c08e6bac78f656c5c0442c359
mayuri-changan <mayuri2001changan@gmail.com>
2026-06-11 00:46:52 +0530

    q20

---
+++ q20.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int power(int x,int y){
+    if(y==0) return 1;
+    return x*power(x,y-1);
+}
+int main(){
+    int x,y; cin>>x>>y;
+    cout<<power(x,y);
+}
