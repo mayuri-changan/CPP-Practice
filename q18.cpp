ed8aacae13b82e44d88cd001ec2f031b20ef7254
mayuri-changan <mayuri2001changan@gmail.com>
2026-06-10 20:58:59 +0530

    q18

---
+++ q18.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n,a=0,b=1,c;
+    cout<<"n:";
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cout<<a<<" ";
+        c=a+b;
+        a=b;
+        b=c;
+    }
+}
