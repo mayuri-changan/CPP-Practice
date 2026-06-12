9280b253caf2cf4cd5c6b2d79f409292f51d7237
mayuri-changan <mayuri2001changan@gmail.com>
2026-06-12 21:02:57 +0530

    23

---
+++ q23.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a[2][3]={{1,2,3},{4,5,6}};
+    for(int j=0;j<3;j++)
+    {
+        for(int i=0;i<2;i++)
+        cout<<a[i][j]<<" ";
+        cout<<endl;
+    }
+}
+
+
+
+
+
+
+
