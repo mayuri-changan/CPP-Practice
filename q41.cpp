5811d6cbb52a69cc905af3afa6c86a8c1a34c5cb
mayuri-changan <mayuri2001changan@gmail.com>
2026-06-18 22:15:44 +0530

    q41

---
+++ q41.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int pin;
+    cout<<"\n-------enter 1st pin----------\n";
+     cin>>pin;
+    while(pin!=50)
+    {    cout<<"===============\nincorrect\n==============";
+        cout<<"\n--------enter again pin-------\n";
+        cin>>pin;
+    
+    }
+            cout<<"==================\nhurrryee!!!!\n=================";
+
+        
+    
+    
+}
+    
+
