c6a8daa417f1a5fb3d3be5ecfe0d416a984fd6a8
mayuri-changan <mayuri2001changan@gmail.com>
2026-06-18 22:12:43 +0530

    q37

---
+++ q39.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter n";
+    cin>>n;
+    
+    for(int i=1;i<=10;i++)
+    {
+        if(n*i)
+        {
+            cout<<n*i;
+        }
+      
+    
+        
+
+    }
+}
