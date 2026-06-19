90c16f9511947595a10670568b0dc0811012fb6c
mayuri-changan <mayuri2001changan@gmail.com>
2026-06-19 14:02:44 +0530

    q42

---
+++ q42.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int pin;
+
+       do{
+cout<<"enter pin";
+cin>>pin;
+       } 
+       while(pin!=50);
+       {
+        cout<<"hurryey";
+       }
+       
+
+       
+    
+    
+}
+    
+
